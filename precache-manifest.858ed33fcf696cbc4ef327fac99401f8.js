8b3e9bf2748bf185ef05ebe530d69bb8668b758e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 14:31:24 +0300

    Updates

---
+++ precache-manifest.858ed33fcf696cbc4ef327fac99401f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31313104a807f6b3946646dc2b6dcde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33db1782a80f7736bb35",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3d1e312743e7d2c20009",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "33db1782a80f7736bb35",
+    "url": "./static/js/2.3f51ff30.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.3f51ff30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1e312743e7d2c20009",
+    "url": "./static/js/main.de923c80.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
